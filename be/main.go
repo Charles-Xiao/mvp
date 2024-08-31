493acef5eed918ddf26a09683bcb40dcd58d2b38
xiaoyong <xiaoyong@baidu.com>
2024-08-31 21:22:23 +0800

    add be

--- be/main.go
+++ be/main.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Vercel!")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", Handler)
 	http.ListenAndServe(":8080", nil)
 }
