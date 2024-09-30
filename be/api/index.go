11c06da5f5e1188f251d63d3aafb5a5355fa0ef3
xiaoyong <xiaoyong@baidu.com>
2024-09-30 16:49:13 +0800

    add be api

--- be/api/index.go
+++ be/api/index.go
@@ -1,15 +1,38 @@
 package handler
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/supabase-community/postgrest-go"
+	"github.com/supabase-community/supabase-go"
 )
 
 // Create a Gin engine instance
 var engine = gin.New()
+var supabaseUrl = os.Getenv("SUPABASE_URL")
+var supabaseKey = os.Getenv("SUPABASE_KEY")
+var supabaseClient *supabase.Client
+
+type Article struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Subtitle  string `json:"subtitle"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	Section   string `json:"section"`
+}
 
 func init() {
+
+	var err error
+	supabaseClient, err = supabase.NewClient(supabaseUrl, supabaseKey, nil)
+	if err != nil {
+		log.Fatalf("Error initializing Supabase client: %v", err)
+	}
+
 	// url: https://mvp-be.vercel.app/hello
 	engine.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -21,6 +44,25 @@ func init() {
 		name := c.DefaultQuery("name", "Guest")
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, " + name + "!",
+		})
+	})
+
+	// url: https://mvp-be.vercel.app/articles
+	engine.GET("/articles", func(c *gin.Context) {
+		var articles []Article
+		_, err := supabaseClient.From("articles").
+			Select("id, title, subtitle, content, created_at, section", "", false).
+			Order("section", &postgrest.OrderOpts{Ascending: true}).
+			ExecuteTo(&articles)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching articles"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data":  articles,
+			"error": nil,
 		})
 	})
 
